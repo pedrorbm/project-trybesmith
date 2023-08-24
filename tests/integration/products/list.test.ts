ad925587c459a995555e9260e377567c184a6158
Pedro Medeiros <pedropwks@gmail.com>
2023-08-24 18:53:21 -0300

    fix: Teste GET /products

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -3,6 +3,7 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../../src/app';
 import productMock from '../../mocks/product.mock';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
@@ -12,10 +13,14 @@ describe('GET /products', function () {
   });
 
   it('Endpoint GET findAll', async function () {
+    const products = productMock.returnGetProducts;
+    const productsReturn = ProductModel.bulkBuild(products);
+    sinon.stub(ProductModel, 'findAll').resolves(productsReturn)
+
     const httpResponse = await chai.request(app).get('/products');
 
     expect(httpResponse.status).to.be.equal(200);
-    expect(httpResponse.body).to.be.deep.equal(productMock.returnGetProducts);
+    expect(httpResponse.body).to.be.deep.equal(products);
   });
 
 });
